06b18c5c0d48bcd4e84cb7ade8fa05aecc1320be
mimmy <norainiibrahim98@gmail.com>
2025-11-16 23:26:25 +0800

    feat: clean up demo template from redux

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,38 +1,32 @@
 import type { Action, ThunkAction } from "@reduxjs/toolkit"
-import { combineSlices, configureStore } from "@reduxjs/toolkit"
-import { setupListeners } from "@reduxjs/toolkit/query"
-import { counterSlice } from "../features/counter/counterSlice"
-import { quotesApiSlice } from "../features/quotes/quotesApiSlice"
+import { configureStore } from "@reduxjs/toolkit"
 
-// `combineSlices` automatically combines the reducers using
-// their `reducerPath`s, therefore we no longer need to call `combineReducers`.
-const rootReducer = combineSlices(counterSlice, quotesApiSlice)
-// Infer the `RootState` type from the root reducer
-export type RootState = ReturnType<typeof rootReducer>
+// NOTE `combineSlices` will be used when we add anime slices. For now, we'll start with an empty root reducer
+const rootReducer = {
+  // TODO: Anime slices will be added here
+}
 
-// The store setup is wrapped in `makeStore` to allow reuse
-// when setting up tests that need the same store config
+// NOTE: Infer the `RootState` type from the root reducer
+export type RootState = typeof rootReducer
+
+// NOTE: The store setup is wrapped in `makeStore` to allow reuse and when setting up tests that need the same store config
 export const makeStore = (preloadedState?: Partial<RootState>) => {
   const store = configureStore({
     reducer: rootReducer,
-    // Adding the api middleware enables caching, invalidation, polling,
-    // and other useful features of `rtk-query`.
+    // TODO: API middleware will be added when we implement anime API
     middleware: getDefaultMiddleware => {
-      return getDefaultMiddleware().concat(quotesApiSlice.middleware)
+      return getDefaultMiddleware()
     },
     preloadedState,
   })
-  // configure listeners using the provided defaults
-  // optional, but required for `refetchOnFocus`/`refetchOnReconnect` behaviors
-  setupListeners(store.dispatch)
   return store
 }
 
 export const store = makeStore()
 
-// Infer the type of `store`
+// NOTE: Infer the type of `store`
 export type AppStore = typeof store
-// Infer the `AppDispatch` type from the store itself
+// NOTE: Infer the `AppDispatch` type from the store itself
 export type AppDispatch = AppStore["dispatch"]
 export type AppThunk<ThunkReturnType = void> = ThunkAction<
   ThunkReturnType,
